9c508aeccdcda6aae14fe42f66bce6fbe665eaa3
Мария Тураева <turaeva-mariya1979@yandex.rugit config --global user.email>
2022-10-02 23:58:35 +0300

    Программа на создание копии заданного массива поэлементным копированием

---
+++ task_45/Program.cs
@@ -0,0 +1,16 @@
+﻿// Задача 45: Напишите программу, которая будет создавать копию заданного массива 
+// с помощью поэлементного копирования.
+
+Console.Clear();
+int[] array = {1, 2, 3, 4, 5};
+int[] b = new int[array.Length];
+Console.WriteLine($"[{string.Join(", ", array)}]");
+Console.WriteLine($"[{string.Join(", ", b)}]");
+
+for (int i = 0; i < array.Length; i++)
+    b[i] = array[i];
+
+b[0] = -10;
+Console.WriteLine($"[{string.Join(", ", array)}]");
+Console.WriteLine($"[{string.Join(", ", b)}]");
+
