63decedb04c72df6d05faff2e09a449cb34776b7
Мария Тураева <turaeva-mariya1979@yandex.rugit config --global user.email>
2022-10-02 23:51:23 +0300

    Программа на преобразование десятичного числа в двоичное (РЕКУРСИЯ)

---
+++ task_42_1/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задача 42: Напишите программу, которая будет преобразовывать десятичное число в двоичное.
+// Решение через РЕКУРСИЮ
+// 45 -> 101101
+// 3 -> 11
+// 2 -> 10
+
+
+Console.Clear();
+
+
+string f(int n)
+{
+    if (n == 0)
+        return "";
+    return f(n / 2) + Convert.ToString(n % 2);
+}
+
+
+Console.Write("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(f(n));
+
