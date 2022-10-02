153630d80bd3c7859379caabffc49820b616dc0b
Мария Тураева <turaeva-mariya1979@yandex.rugit config --global user.email>
2022-10-02 23:49:32 +0300

    Программа на преобразование десятичного числа в двоичное

--- task_42/Program.cs
+++ task_42/Program.cs
@@ -19,19 +19,3 @@ while (n > 0)
 Console.WriteLine(s);
 
 n++;
-
- /*Console.Clear();
-
-
-string f(int n)
-{
-    if (n == 0)
-        return "";
-    return f(n / 2) + Convert.ToString(n % 2);
-}
-
-
-Console.Write("Введите число: ");
-int n = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine(f(n));
-*/
